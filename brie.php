bf2ce2fbc4c455241cb8135ddaa1dddd794f7dca
Unknown <Co2Noss@gmail.com>
2017-10-27 02:52:53 -0400

    2.002
    Brie.php update

--- brie.php
+++ brie.php
@@ -12,59 +12,22 @@ if(!$fgmembersite->CheckLogin())
   -- Contributers:
   -- Charles Yost-->
 
-<!doctype html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
   <html>
   <head>
-      <title>Template Ace</title>
+      <title>Template Ace 2.0</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-  <style type="text/css">
-      
-      html { width: 100%; height:100%; overflow:hidden; }
-body {
-	width: 100%;
-	height:100%;
-	font-family: 'Roboto', sans-serif;
-	background: #092756;
-	background-image: url(bckgrnd.jpg);
-    background-size: cover;
-    background-position: 50% 50%
-}
-#top {
-	background: url(bckgrndtop.jpg) center; 
-	background-size: 100% 100%;
-	width:100%;
-	max-width: 710px;
-	margin:0 auto;
-	padding: 10% 5%;
-}
-		
-a{
- text-decoration:none;
- color:#cc0000;
-}
-a.button,div.container{
- float:none;
-}
-      .button:hover{
-           background:#606060;
-          box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-                fill: #eeeeee;
-                cursor:pointer;
-      }
-a.button{
- background-color:#0D3036;
- border-radius:2px;
- color:#fff;
- margin-right:10px;
- padding:10px 10px 10px 10px;
-}
-      div.container{
- width:100%;
- clear:both;
-}
-	 
-  </style><!-- Piwik -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+		<title>Template Ace</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+<!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
@@ -79,20 +42,17 @@ a.button{
   })();
 </script>
 <!-- End Piwik Code -->
-     </head>
-    
-  <body>
-      <?php include_once("analyticstracking.php") ?>
-      <div id="top"></div>
-  <div align="center" class="container">	 <br /><br /><br /><br /><br /><br /> 
+	</head>
+	<body><div align="center" class="container" align="center" id="login"><br /><br /><br />
 <a href="https://templateace.xyz/nt.php" class="button">Official Note Template</a>
      <a href="https://templateace.xyz/final.php" class="button">Final Incident</a>
      <a href="https://templateace.xyz/mm.php" class="button">Mentor Machine 2.0</a>
     <a href="https://templateace.xyz/history/home/" class="button">Note Template; A History [Blog]</a>
 </div>
-	  
-	
-    
-  </body>
-  </html>
+	<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
 
